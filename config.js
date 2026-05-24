ee9be30f1ab9aae0845f707bb5795a22892ff986
SAMUEL-HACK1 <cuejtacuban@gmail.com>
2026-05-24 18:31:37 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+// config.js - Samuel Hack Store
+module.exports = {
+    telegramToken: "8821565440:AAGCiPT9AmSodGWQYKDtIhluGQz5U7aQeIg",
+    adminTelegramId: 8698439117,
+    adminWaNumbers: ["584166371131"],
+    firebaseConfig: {
+        apiKey: "AIzaSyB-J-8BUFhnDcZIEvYRNq6UAFaPcgxuAtE",
+        authDomain: "saku-store.firebaseapp.com",
+        databaseURL: "https://saku-store-default-rtdb.firebaseio.com",
+        projectId: "saku-store",
+        storageBucket: "saku-store.firebasestorage.app",
+        messagingSenderId: "815286002143",
+        appId: "1:815286002143:web:60e7c7770da491a6da646a",
+        measurementId: "G-W9EHSXZJVT"
+    }
+};
